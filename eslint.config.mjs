4b42a795d492fd187992dddf6d6245b9ca48be6b
Ahsan Najmy Ramadhan Putra Aji <ahsan@Ahsans-MacBook-Pro.local>
2024-08-11 13:59:48 +0700

    edit gitignore

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-// import dastyle from 'eslint-config-dicodingacademy';
-
-export default [
-  // dastyle,
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-];
